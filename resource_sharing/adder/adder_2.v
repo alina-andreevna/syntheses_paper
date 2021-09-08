579eeaea11c7d8e03408c71b36fccd2aa4a1311c
Alina Galichina <ali-piratova@mail.ru>
2021-09-08 20:44:26 +0300

    fixed typo

--- resource_sharing/adder/adder_2.v
+++ resource_sharing/adder/adder_2.v
@@ -4,7 +4,7 @@ module adder_2 (
     input [1:0] i_sel);
     wire add_input [0:1];
     
-    assign add_input[0] = (i_sel == 2) ? i_data[0] : i_data[1];
+    assign add_input[0] = (i_sel == 2) ? i_data[1] : i_data[0];
     assign add_input[1] = (i_sel == 0) ? i_data[1] : (i_sel == 3) ? 0 : i_data[2];
     assign o_data = add_input[0] + add_input[1];
 endmodule : adder_2
